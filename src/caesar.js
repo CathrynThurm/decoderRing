e6c4b2426cc7533c0801c1c9a80eac31422177c7
Cathryn Thurm <66237406+CathrynThurm@users.noreply.github.com>
2020-12-29 18:03:36 -0500

    Update caesar.js

--- src/caesar.js
+++ src/caesar.js
@@ -1,12 +1,17 @@
+// This creates an array of the alphabet to be referenced in several different functions
 const alph = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 
-
+// this is the main function that calls all other functions
 function caesar(input, shift, encode = true) {
+    
+    // exits the function if the shift input is too high, too low, or not present
     if(!shift || shift < -25 || shift > 25) {return false}
-
+    
+    // This converts all letters in input into lowercase letters
     input = input.toLowerCase()
     let result = ""
-
+    
+    // calls either encodeFunc or decodeFunc based on the encode param
     if(encode) {result = encodeFunc(input, shift)}
     else{ result = decodeFunc(input, shift)}
 
@@ -14,12 +19,15 @@ function caesar(input, shift, encode = true) {
     return result
 }
 
+// This function gets called when the encode param in caesar is true
+// This function encodes the input param
 function encodeFunc(input, shift) {
     let result = ""
     let amountLeft =0
     let newShift = 0
 
     for(let i = 0; i < input.length; i++) {
+        // checks if any letter in input is a space or special character
         if(!input[i].match(/[a-z]/)){ result += input[i]}
 
         for(let j = 0; j < alph.length; j++) {
@@ -41,10 +49,13 @@ function encodeFunc(input, shift) {
     return result
 }
 
+// This function gets called when the encode param in caesar is false
+// This function decodes the input param
 function decodeFunc(input, shift) {
     let result =""
 
     for(let i = 0; i < input.length; i++) {
+        // checks if any letter in input is a space or special character
         if(!input[i].match(/[a-z]/)){ result += input[i]}
 
         for(let j = 0; j < alph.length; j++) {
